b79874a1b28e1a2bd1e75bb1e02bf48c076c4d4c
ThanGrove <than@virginia.edu>
2014-09-05 17:19:08 -0400

    Updating to add all of Kmaps JS and to insert main content markup when kmap is clicked

---
+++ templates/shanti_kmap_page.tpl.php
@@ -0,0 +1,107 @@
+<article class="main-content kmap-template" role="main">
+	
+  <!-- Two Columns: content-resources and content-section -->
+  <div class="row row-offcanvas row-offcanvas-left">
+  
+    <!-- Column Resources  -->              
+    <aside class="content-resources col-xs-6 col-sm-3 sidebar-offcanvas equal-height">
+      <ul class="nav nav-pills nav-stacked">
+        <li class="overview"><a href="#tab-overview" data-toggle="pill">
+          <i class="icon shanticon-overview"></i>Overview</a>
+        </li>
+        <li class="subjects"><a href="#tab-subjects" data-toggle="pill">
+          <i class="icon shanticon-subjects"></i>Subjects<span class="badge"></span></a>
+        </li>
+        <li class="essays"><a href="#tab-essays" data-toggle="pill">
+          <i class="icon shanticon-essays"></i>Essays<span class="badge"></span></a>
+        </li>
+        <li class="places"><a href="#tab-places" data-toggle="pill">
+          <i class="icon shanticon-places"></i>Places<span class="badge"></span></a>
+        </li>
+        <li class="agents"><a href="#tab-agents" data-toggle="pill">
+          <i class="icon shanticon-agents"></i>Agents<span class="badge"></span></a>
+        </li>
+        <li class="events"><a href="#tab-events" data-toggle="pill">
+          <i class="icon shanticon-events"></i>Events<span class="badge"></span></a>
+        </li>
+        <li class="photos"><a href="#tab-photos" data-toggle="pill">
+          <i class="icon shanticon-photos"></i>Photos<span class="badge"></span></a>
+        </li>
+        <li class="audio-video"><a href="#tab-audio-video" data-toggle="pill">
+          <i class="icon shanticon-audio-video"></i>Audio-Video<span class="badge"></span></a>
+        </li>
+        <li class="visuals"><a href="#tab-visuals" data-toggle="pill">
+          <i class="icon shanticon-visuals"></i>Visuals<span class="badge"></span></a>
+        </li>
+        <li class="texts"><a href="#tab-texts" data-toggle="pill">
+          <i class="icon shanticon-texts"></i>Texts<span class="badge"></span></a>
+        </li>
+        <li class="maps"><a href="#tab-maps" data-toggle="pill">
+          <i class="icon shanticon-maps"></i>Maps<span class="badge"></span></a>
+        </li>
+        <li class="community"><a href="#tab-community" data-toggle="pill">
+          <i class="icon shanticon-community"></i>Community<span class="badge"></span></a>
+        </li>
+        <li class="terms"><a href="#tab-terms" data-toggle="pill">
+          <i class="icon shanticon-terms"></i>Terms<span class="badge"></span></a>
+        </li>
+        <li class="sources"><a href="#tab-sources" data-toggle="pill">
+          <i class="icon shanticon-sources"></i>Sources<span class="badge"></span></a>
+        </li>
+      </ul> 
+    </aside> 
+  
+    <!-- Column Main  -->                   
+    <section  class="content-section col-xs-12 col-sm-9 equal-height">
+      
+      <div class="tab-content">
+      
+        <article class="tab-pane" id="tab-overview">
+        </article>
+
+        <article class="tab-pane" id="tab-subjects"> 
+        </article>
+
+        <article class="tab-pane" id="tab-essays">
+        </article>
+
+        <article class="tab-pane" id="tab-places">
+        </article>
+                        
+        <article class="tab-pane" id="tab-agents">
+        </article>
+
+        <article class="tab-pane" id="tab-events">
+        </article>
+
+        <article class="tab-pane" id="tab-photos">
+        </article>
+
+        <article class="tab-pane" id="tab-audio-video">
+        </article>
+
+        <article class="tab-pane" id="tab-visuals">
+        </article>
+
+        <article class="tab-pane" id="tab-texts">
+        </article>
+
+        <article class="tab-pane" id="tab-maps">
+        </article>
+
+        <article class="tab-pane" id="tab-community">
+        </article>
+
+        <article class="tab-pane" id="tab-terms">
+        </article>
+
+        <article class="tab-pane" id="tab-sources">
+        </article>
+
+        <article class="show-related-pages">
+        </article>
+      
+      </div><!-- END tab-content -->
+    </section><!-- END content-section -->                
+  </div><!-- END row -->    
+</article><!-- END main-content -->
