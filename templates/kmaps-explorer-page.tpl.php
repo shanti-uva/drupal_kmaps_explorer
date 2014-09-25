9bd286d6c3639a5ac948f5d01f334303cdc27709
ThanGrove <than@virginia.edu>
2014-09-25 11:25:02 -0400

    Renaming to kmaps_explorer

---
+++ templates/kmaps-explorer-page.tpl.php
@@ -0,0 +1,91 @@
+<article class="main-content kmap-template" role="main">
+	
+  <!-- Two Columns: content-resources and content-section -->
+  <div class="row row-offcanvas row-offcanvas-left">
+  
+    <!-- Column Resources  -->              
+    <aside class="content-resources col-xs-6 col-sm-3 sidebar-offcanvas equal-height">
+      <ul class="nav nav-pills nav-stacked">
+        <li class="overview"><a href="#tab-overview" data-toggle="pill">
+          <i class="icon shanticon-overview"></i>Overview</a>
+        </li>
+        <li class="subjects"><a href="#tab-subjects" data-toggle="pill">
+          <i class="icon shanticon-subjects"></i>Subjects<span class="badge"></span></a>
+        </li>
+        <li class="essays"><a href="#tab-essays" data-toggle="pill">
+          <i class="icon shanticon-essays"></i>Essays<span class="badge"></span></a>
+        </li>
+        <li class="places"><a href="#tab-places" data-toggle="pill">
+          <i class="icon shanticon-places"></i>Places<span class="badge"></span></a>
+        </li>
+        <li class="agents"><a href="#tab-agents" data-toggle="pill">
+          <i class="icon shanticon-agents"></i>Agents<span class="badge"></span></a>
+        </li>
+        <li class="events"><a href="#tab-events" data-toggle="pill">
+          <i class="icon shanticon-events"></i>Events<span class="badge"></span></a>
+        </li>
+        <li class="photos"><a href="#tab-photos" data-toggle="pill">
+          <i class="icon shanticon-photos"></i>Photos<span class="badge"></span></a>
+        </li>
+        <li class="audio-video"><a href="#tab-audio-video" data-toggle="pill">
+          <i class="icon shanticon-audio-video"></i>Audio-Video<span class="badge"></span></a>
+        </li>
+        <li class="visuals"><a href="#tab-visuals" data-toggle="pill">
+          <i class="icon shanticon-visuals"></i>Visuals<span class="badge"></span></a>
+        </li>
+        <li class="texts"><a href="#tab-texts" data-toggle="pill">
+          <i class="icon shanticon-texts"></i>Texts<span class="badge"></span></a>
+        </li>
+        <li class="maps"><a href="#tab-maps" data-toggle="pill">
+          <i class="icon shanticon-maps"></i>Maps<span class="badge"></span></a>
+        </li>
+        <li class="community"><a href="#tab-community" data-toggle="pill">
+          <i class="icon shanticon-community"></i>Community<span class="badge"></span></a>
+        </li>
+        <li class="terms"><a href="#tab-terms" data-toggle="pill">
+          <i class="icon shanticon-terms"></i>Terms<span class="badge"></span></a>
+        </li>
+        <li class="sources"><a href="#tab-sources" data-toggle="pill">
+          <i class="icon shanticon-sources"></i>Sources<span class="badge"></span></a>
+        </li>
+      </ul> 
+    </aside> 
+  
+    <!-- Column Main  -->                   
+    <section  class="content-section col-xs-12 col-sm-9 equal-height">
+      <div class="tab-content">
+          
+            <article class="tab-pane active" id="tab-overview"><h6>Collections</h6><p>Collections are used to articulate collections and subcollections which then can be used to index and present audio-video, photographs, texts, visualizations, and so forth as connected together into collections. Thus one might have an "Architecture of Ukraine" collection, with subcollections devoted to Churches, public buildings, etc.&nbsp;</p><figure class="cap-bot"><img src="http://mms.thlib.org/images/0053/7241/67458_large.jpg" class="img-responsive img-thumbnail" alt="The assembly hall"><figcaption><div class="center-caption">The assembly hall</div><p>Shaba Ridzong is about 28 kilometer away from Ping'an; the County seat, Haidong Region, Qinghai Provice. This is one of four great Dzong in Amdo; they are Achong Nam Dzong, Pula Yangdzong, Dragkhar Dral Dzong and Shawa Ridzong. In the year of 1359, The fourth reincarnated Lama, the black hatted Karma Rolpe Dorje stayed at this monatery on his way to Beijing to with the invitation of King of Yuan dynasty. More importantly, when Zongkapa was three years old, he tool his religous vows in front of Choje Dondrup Rinchen and named him Ananda or Kunga Ningbo. This is the roof of the great assembly hall.</p></figcaption></figure></article>
+
+            <article class="tab-pane" id="tab-subjects"></article>
+
+            <article class="tab-pane" id="tab-essays"></article>
+
+            <article class="tab-pane" id="tab-places"></article>
+                            
+            <article class="tab-pane" id="tab-agents"></article>
+
+            <article class="tab-pane" id="tab-events"></article>
+
+            <article class="tab-pane" id="tab-photos"></article>
+
+            <article class="tab-pane" id="tab-audio-video"></article>
+
+            <article class="tab-pane" id="tab-visuals"></article>
+
+            <article class="tab-pane" id="tab-texts"></article>
+
+            <article class="tab-pane" id="tab-maps"></article>
+
+            <article class="tab-pane" id="tab-community"></article>
+
+            <article class="tab-pane" id="tab-terms"></article>
+
+            <article class="tab-pane" id="tab-sources"></article>
+
+            <article class="show-related-pages"></article>
+          
+      </div><!-- END tab-content -->
+    </section><!-- END content-section -->                
+  </div><!-- END row -->    
+</article><!-- END main-content -->
