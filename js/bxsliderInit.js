8ba5640d85a679f76cdcb62adab186562f7f5372
Gerard Ketuma <me@gerardketuma.com>
2014-12-05 23:00:49 -0600

    Implement homepage for places

---
+++ js/bxsliderInit.js
@@ -0,0 +1,23 @@
+(function ($) {
+  Drupal.behaviors.kmapsExplorer = {
+    attach: function (context, settings) {
+      var $selected_li = $('#carousel-feature-slides > li');
+      $selected_li.children('a').bind('click', function (e) {
+          e.preventDefault();
+          $selected_li.removeClass('active');
+          $(this).parent().addClass('active');
+      });
+
+      $('.bxslider').bxSlider({
+        slideMargin:10,
+        pager:true,
+        controls:true,
+        autoReload: true,
+        moveSlides: 1,
+        breaks: [{screen:0, slides:1, pager:false},{screen:380, slides:1},{screen:450, slides:2},{screen:768, slides:3},{screen:1200, slides:4}]
+      });
+    }
+  };
+
+
+})(jQuery);
--- templates/places/kmaps-explorer-places-landing-page.tpl.php
+++ templates/places/kmaps-explorer-places-landing-page.tpl.php
@@ -1,3 +1,296 @@
 <div id="places-main">
-  <p>This is the landing page for the places app.</p>
-</div>
+  <div role="tabpanel">
+    <div class="tabcontent">
+
+      <div id="slide-one" class="feature-slide tab-pane fade active in">
+        <div class="carousel-header go-to-link">
+          <span>Featured Places: Tibet Autonomous Region</span>
+          <!-- <a href="/collections">View All Collections</a> -->
+        </div>
+
+          <div id="carousel-feature-detail" class="carousel-feature-detail">
+          <div class="container-fluid">
+            <div class="row">
+                  <div class="carousel-feature-detail-content col-xs-12 col-sm-6 col-md-5">
+                    <div><h3 class="carousel-feature-title">
+                      <a href="collection/tibetan-and-himalayan-library">
+                       <span class="icon shanticon-stack"></span>
+                          Chamdo Prefecture
+                      </a>
+                      </h3>
+                    </div>
+                    <!-- <p class="carousel-feature-byline">AUTHOR</p> -->
+                    <p class="carousel-feature-description">Chamdo officially Qamdo is a prefecture-level city in the eastern part of the Tibet Autonomous Region. Its seat is the town of Chengguan in Karub District. Chamdo is Tibet's third largest city after Lhasa and Shigatse. Within China, Tibet is identified with the Autonomous Region. The current borders of Tibet were generally established in the 18th century[4] and include about half of ethno-cultural Tibet. The Tibet Autonomous Region is the second-largest province-level division...</p>
+                    <!-- <p class="go-to-link"><a href="#" class="h5">LINK TITLE</a></p> -->
+                  </div>
+                  <div class="carousel-related-resource-group col-xs-12 col-sm-6 col-md-7">
+                    <div class="row">
+                        <div class="col-xs-6 col-md-4 carousel-resource-item">
+                          <a href="">
+                          <span><img src="<?php print $path; ?>/places/chamdo_1b.jpg"></span>
+                            <!-- <h6>DATE</h6> -->
+                            <h5>Karub District</h5>
+                            <p>Karub District (other spellings: Karuo District, Kharro District) is a district in Qamdo, the district is...</p>
+                          </a>
+                        </div>
+                        <div class="col-xs-6 col-md-4 carousel-resource-item">
+                          <a href="">
+                          <span><img src="<?php print $path; ?>/places/chamdo_2b.jpg"></span>
+                            <!-- <h6>DATE</h6> -->
+                            <h5>Baxoi County</h5>
+                            <p>Baxoi County, occasionally transliterated from Tibetan as Pashoe County. It contains the Pomda...</p>
+                          </a>
+                        </div>
+                        <div class="hidden-xs hidden-sm col-md-4 carousel-resource-item">
+                          <a href="">
+                          <span><img src="<?php print $path; ?>/places/chamdo_3b.jpg"></span>
+                            <!-- <h6>DATE</h6> -->
+                            <h5>Markum County</h5>
+                            <p>Markham County is a county of the Chamdo Prefecture in the Tibet Autonomous Region...</p>
+                          </a>
+                        </div>
+                    </div>
+               <!-- <div class="row feature-detail-assets">
+                        <div class="col-xs-12 col-sm-4"><a href="#"><span class="icon shanticon-audio-video"></span><strong>1200</strong> Titles with Video</a></div>
+                        <div class="col-xs-12 col-sm-4"><a href="#"><span class="icon shanticon-audio"></span><strong>821</strong> Titles with Audio</a></div>
+                        <div class="col-xs-12 col-sm-4"><a href="#"><span class="icon shanticon-create"></span><strong>253</strong> Titles with Transcripts</a></div>
+                    </div> -->
+                  </div>
+            </div>
+          </div>
+        </div>
+      </div><!-- END slide one -->
+
+
+      <div id="slide-two" class="feature-slide tab-pane fade">
+        <div class="carousel-header go-to-link">
+          <span>Featured Places: Tibet Autonomous Region</span>
+          <!-- <a href="/collections">View All Collections</a> -->
+        </div>
+
+          <div id="carousel-feature-detail" class="carousel-feature-detail">
+          <div class="container-fluid">
+            <div class="row">
+                  <div class="carousel-feature-detail-content col-xs-12 col-sm-6 col-md-5">
+                    <div><h3 class="carousel-feature-title">
+                      <a href="collection/tibetan-and-himalayan-library">
+                       <span class="icon shanticon-stack"></span>
+                          Lhasa Prefecture
+                      </a>
+                      </h3>
+                    </div>
+                    <!-- <p class="carousel-feature-byline">AUTHOR</p> -->
+                    <p class="carousel-feature-description">Lhasa is the administrative capital of Tibet Autonomous Region. It is the second most populous city on the Tibetan Plateau after Xining and, at an altitude of 3,490 metres (11,450 ft), Lhasa is one of the highest cities in the world. The city contains many culturally significant Tibetan Buddhist sites such as the Potala Palace, Jokhang temple and Norbulingka palaces, many of which are located in Chengguan District, the city seat. By the mid 7th century, Songtsän Gampo became the leader...</p>
+                    <!-- <p class="go-to-link"><a href="#" class="h5">LINK TITLE</a></p> -->
+                  </div>
+                  <div class="carousel-related-resource-group col-xs-12 col-sm-6 col-md-7 ">
+                    <div class="row">
+                        <div class="col-xs-6 col-md-4 carousel-resource-item">
+                          <a href="">
+                          <span><img src="<?php print $path; ?>/places/lhasa_1b.jpg"></span>
+                            <!-- <h6>DATE</h6> -->
+                            <h5>Chengguan District</h5>
+                            <p>Chengguan is part of the Lhasa City which consists of seven small...</p>
+                          </a>
+                        </div>
+                        <div class="col-xs-6 col-md-4 carousel-resource-item">
+                          <a href="">
+                          <span><img src="<?php print $path; ?>/places/lhasa_2b.jpg"></span>
+                            <!-- <h6>DATE</h6> -->
+                            <h5>Doilungdêgên County</h5>
+                            <p>Doilungdêgên County is located in central and southern Tibet and is a...</p>
+                          </a>
+                        </div>
+                        <div class="hidden-xs hidden-sm col-md-4 carousel-resource-item">
+                          <a href="">
+                          <span><img src="<?php print $path; ?>/places/lhasa_3b.jpg"></span>
+                            <!-- <h6>DATE</h6> -->
+                            <h5>Lhünzhub County</h5>
+                            <p>Lhünzhub County is located in central Tibet in the Peng Po Valley, around 65 km northeast...</p>
+                          </a>
+                        </div>
+                    </div>
+               <!-- <div class="row feature-detail-assets">
+                        <div class="col-xs-12 col-sm-4"><a href="#"><span class="icon shanticon-audio-video"></span><strong>1200</strong> Titles with Video</a></div>
+                        <div class="col-xs-12 col-sm-4"><a href="#"><span class="icon shanticon-audio"></span><strong>821</strong> Titles with Audio</a></div>
+                        <div class="col-xs-12 col-sm-4"><a href="#"><span class="icon shanticon-create"></span><strong>253</strong> Titles with Transcripts</a></div>
+                    </div> -->
+                  </div>
+            </div>
+          </div>
+        </div>
+      </div><!-- END slide two -->
+
+
+      <div id="slide-three" class="feature-slide tab-pane fade">
+        <div class="carousel-header go-to-link">
+          <span>Featured Places: Tibet Autonomous Region</span>
+          <!-- <a href="/collections">View All Collections</a> -->
+        </div>
+
+          <div id="carousel-feature-detail" class="carousel-feature-detail">
+          <div class="container-fluid">
+            <div class="row">
+                  <div class="carousel-feature-detail-content col-xs-12 col-sm-6 col-md-5">
+                    <div><h3 class="carousel-feature-title">
+                      <a href="collection/tibetan-and-himalayan-library">
+                       <span class="icon shanticon-stack"></span>
+                          Nyingchi Prefecture
+                      </a>
+                      </h3>
+                    </div>
+                    <!-- <p class="carousel-feature-byline">AUTHOR</p> -->
+                    <p class="carousel-feature-description">Nyingchi Prefecture is a prefecture in southeast of the Tibet Autonomous Region. The administrative seat of Nyingchi Prefecture is Bayi. The Chinese claim part of Arunachal Pradesh, which is one of the states of India, as part of the prefecture. The prefecture contains the Namcha Barwa Peak, and the Yarlung Tsangpo Grand Canyon, located in Mêdog County, which attracts significant tourism and geoscientific and biological study to the area.</p>
+                    <!-- <p class="go-to-link"><a href="#" class="h5">LINK TITLE</a></p> -->
+                  </div>
+                  <div class="carousel-related-resource-group col-xs-12 col-sm-6 col-md-7 ">
+                    <div class="row">
+                        <div class="col-xs-6 col-md-4 carousel-resource-item">
+                          <a href="">
+                          <span><img src="<?php print $path; ?>/places/nyingchi_1b.jpg"></span>
+                            <!-- <h6>DATE</h6> -->
+                            <h5>Nyingchi County</h5>
+                            <p>Nyingchi County is located in the middle reaches of the Yarlung Tsangpo River. Both steep cliffs...</p>
+                          </a>
+                        </div>
+                        <div class="col-xs-6 col-md-4 carousel-resource-item">
+                          <a href="">
+                          <span><img src="<?php print $path; ?>/places/nyingchi_2b.jpg"></span>
+                            <!-- <h6>DATE</h6> -->
+                            <h5>Gongbo'gyamda County</h5>
+                            <p>Gongbo'gyamda County is located in the east of the Tibet Autonomous Region, to the south of...</p>
+                          </a>
+                        </div>
+                        <div class="hidden-xs hidden-sm col-md-4 carousel-resource-item">
+                          <a href="">
+                          <span><img src="<?php print $path; ?>/places/nyingchi_3b.jpg"></span>
+                            <!-- <h6>DATE</h6> -->
+                            <h5>Bomê County</h5>
+                            <p>Bomai was the seat of a quasi-independent kingdom until the early 20th century when troops of ...</p>
+                          </a>
+                        </div>
+                    </div>
+               <!-- <div class="row feature-detail-assets">
+                        <div class="col-xs-12 col-sm-4"><a href="#"><span class="icon shanticon-audio-video"></span><strong>1200</strong> Titles with Video</a></div>
+                        <div class="col-xs-12 col-sm-4"><a href="#"><span class="icon shanticon-audio"></span><strong>821</strong> Titles with Audio</a></div>
+                        <div class="col-xs-12 col-sm-4"><a href="#"><span class="icon shanticon-create"></span><strong>253</strong> Titles with Transcripts</a></div>
+                    </div> -->
+                  </div>
+            </div>
+          </div>
+        </div>
+      </div><!-- END slide three -->
+
+
+      <div id="slide-four" class="feature-slide tab-pane fade">
+        <div class="carousel-header go-to-link">
+          <span>Featured Places: Tibet Autonomous Region</span>
+          <!-- <a href="/collections">View All Collections</a> -->
+        </div>
+
+          <div id="carousel-feature-detail" class="carousel-feature-detail">
+          <div class="container-fluid">
+            <div class="row">
+                  <div class="carousel-feature-detail-content col-xs-12 col-sm-6 col-md-5">
+                    <div><h3 class="carousel-feature-title">
+                      <a href="collection/tibetan-and-himalayan-library">
+                       <span class="icon shanticon-stack"></span>
+                          Ngari Prefecture
+                      </a>
+                      </h3>
+                    </div>
+                    <!-- <p class="carousel-feature-byline">AUTHOR</p> -->
+                    <p class="carousel-feature-description">Ngari Prefecture (Tibetan: མངའ་རིས་ས་ཁུལ་, Wylie: mnga' ris sa khul; simplified Chinese: 阿里地区; traditional Chinese: 阿里地區; pinyin: Ālǐ Dìqū) is a prefecture of China's Tibet Autonomous Region. Its capital is Gar County. Its administrative centre is the town of Burang Town. The largest settlement is Shiquanhe.The paved Xinjiang-Tibet Highway (新藏公路) passes through this area. There are well-known prehistoric petroglyphs near the far western town of Rutog...</p>
+                    <!-- <p class="go-to-link"><a href="#" class="h5">LINK TITLE</a></p> -->
+                  </div>
+                  <div class="carousel-related-resource-group col-xs-12 col-sm-6 col-md-7 ">
+                    <div class="row">
+                        <div class="col-xs-6 col-md-4 carousel-resource-item">
+                          <a href="">
+                          <span><img src="<?php print $path; ?>/places/ngari_1b.jpg"></span>
+                            <!-- <h6>DATE</h6> -->
+                            <h5>Rutog County</h5>
+                            <p>The county has a rich history of folk tales, myths, legends, proverbs and folk songs and has...</p>
+                          </a>
+                        </div>
+                        <div class="col-xs-6 col-md-4 carousel-resource-item">
+                          <a href="">
+                          <span><img src="<?php print $path; ?>/places/ngari_2b.jpg"></span>
+                            <!-- <h6>DATE</h6> -->
+                            <h5>Burang County</h5>
+                            <p>Burang County has TAR's south-western border with Nepal's Karnali Zone, Humla District...</p>
+                          </a>
+                        </div>
+                        <div class="hidden-xs hidden-sm col-md-4 carousel-resource-item">
+                          <a href="">
+                          <span><img src="<?php print $path; ?>/places/ngari_3b.jpg"></span>
+                            <!-- <h6>DATE</h6> -->
+                            <h5>Gar County</h5>
+                            <p>Gar County is a county in the Ngari Prefecture of the western Tibet Autonomous Region of China...</p>
+                          </a>
+                        </div>
+                    </div>
+               <!-- <div class="row feature-detail-assets">
+                        <div class="col-xs-12 col-sm-4"><a href="#"><span class="icon shanticon-audio-video"></span><strong>1200</strong> Titles with Video</a></div>
+                        <div class="col-xs-12 col-sm-4"><a href="#"><span class="icon shanticon-audio"></span><strong>821</strong> Titles with Audio</a></div>
+                        <div class="col-xs-12 col-sm-4"><a href="#"><span class="icon shanticon-create"></span><strong>253</strong> Titles with Transcripts</a></div>
+                    </div> -->
+                  </div>
+            </div>
+          </div>
+        </div>
+      </div><!-- END slide four -->
+
+    </div><!-- END tab-content -->
+
+
+<!-- small boxes  <ul id="carousel-feature-slides" class="carousel-feature-slides bxslider" role="tablist" data-call="bxslider" data-options="{slideMargin:5, autoReload:true, pager:true, controls:true}" data-breaks="[{screen:0, slides:1, pager:false},{screen:400, slides:2},{screen:550, slides:3},{screen:768, slides:4},{screen:1050, slides:5},{screen:1200, slides:6}]"> -->
+
+<!-- large slides -->
+          <ul id="carousel-feature-slides" class="carousel-feature-slides bxslider" role="tablist">
+            <li class="active" role="presentation">
+              <div>
+                <a href="#slide-one" aria-controls="slide-one" role="tab" data-toggle="tab">
+                <img src="<?php print $path; ?>/places/chamdo_map.png">
+                <p><strong>Name:</strong> Chamdo Prefecture<br>
+                  <strong>Place ID:</strong> F15359
+                </p>
+                </a>
+              </div>
+            </li>
+            <li role="presentation">
+              <div>
+                <a href="#slide-two" aria-controls="slide-two" role="tab" data-toggle="tab">
+                <img src="<?php print $path; ?>/places/lhasa_map.png">
+                <p><strong>Name:</strong> Lhasa Prefecture-level City<br>
+                  <strong>Place ID:</strong> F17632
+                </p>
+                </a>
+              </div>
+            </li>
+            <li role="presentation">
+              <div>
+                <a href="#slide-three" aria-controls="slide-three" role="tab" data-toggle="tab">
+                <img src="<?php print $path; ?>/places/nyingchi_map.png">
+                <p><strong>Name:</strong> Nyingchi Prefecture<br>
+                  <strong>Place ID:</strong> F179
+                </p>
+                </a>
+              </div>
+            </li>
+            <li role="presentation">
+              <div>
+                <a href="#slide-four" aria-controls="slide-four" role="tab" data-toggle="tab">
+                <img src="<?php print $path; ?>/places/ngari_map.png">
+                <p>
+                  <strong>Name:</strong> Ngari Prefecture<br>
+                  <strong>Place ID:</strong> F7981
+                </p>
+                </a>
+              </div>
+            </li>
+        </ul>
+
+
+  </div> <!-- END tab-panel -->
+</div><!-- End of places-main -->
