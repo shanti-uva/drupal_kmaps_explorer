8048e383d440c5d1f61925aaf99c324904a61b33
mferrara <mf8yk@virginia.edu>
2014-12-13 18:32:44 -0600

    carousel set default active slide, anchors #

--- templates/subjects/kmaps-explorer-subjects-landing-page.tpl.php
+++ templates/subjects/kmaps-explorer-subjects-landing-page.tpl.php
@@ -2,17 +2,17 @@
   <div class="feature-carousel-tabpanel" role="tabpanel">
     <div class="tabcontent">
 
-      <div id="slide-one" class="feature-slide tab-pane fade">
+      <div id="slide-one" class="feature-slide tab-pane fade active in">
         <div class="carousel-header go-to-link">
-          <span>Featured Places: Tibet Autonomous Region</span>
-          <a href="/collections">View All Collections</a>
+          <span>Featured Subjects: Tibet Autonomous Region</span>
+          <a href="#">View All Collections</a>
         </div>
         <div id="feature-slide-detail" class="feature-slide-detail">
           <div class="container-fluid">
             <div class="row">
                   <div class="feature-slide-detail-content col-xs-12 col-sm-7 col-md-6 col-lg-7">
                     <div><h3 class="feature-slide-title">
-                      <a href="collection/tibetan-and-himalayan-library">
+                      <a href="#">
                        <span class="icon shanticon-stack"></span>
                           Personal Collections
                       </a>
@@ -64,14 +64,14 @@
       <div id="slide-two" class="feature-slide tab-pane fade">
         <div class="carousel-header go-to-link">
           <span>Featured Places: Tibet Autonomous Region</span>
-          <a href="/collections">View All Collections</a>
+          <a href="#">View All Collections</a>
         </div>
         <div id="feature-slide-detail" class="feature-slide-detail">
           <div class="container-fluid">
             <div class="row">
                   <div class="feature-slide-detail-content col-xs-12 col-sm-7 col-md-6 col-lg-7">
                     <div><h3 class="feature-slide-title">
-                      <a href="collection/tibetan-and-himalayan-library">
+                      <a href="#">
                        <span class="icon shanticon-stack"></span>
                           Provisional Collections
                       </a>
@@ -84,7 +84,7 @@
                   <div class="carousel-related-resource-group col-xs-12 col-sm-5 col-md-6 col-lg-5 ">
                     <div class="row">
                         <div class="col-xs-6 col-md-4 carousel-resource-item">
-                          <a href="">
+                          <a href="#">
                           <span><img src="<?php print $path; ?>/subjects/provisional-1.jpg"></span>
                             <h6>January 1, 2015</h6>
                             <h5>1999-digital</h5>
@@ -92,7 +92,7 @@
                           </a>
                         </div>
                         <div class="col-xs-6 col-md-4 carousel-resource-item">
-                          <a href="">
+                          <a href="#">
                           <span><img src="<?php print $path; ?>/subjects/provisional-2.jpg"></span>
                             <h6>January 1, 2015</h6>
                             <h5>Geotourism</h5>
@@ -100,7 +100,7 @@
                           </a>
                         </div>
                         <div class="hidden-xs hidden-sm col-md-4 carousel-resource-item">
-                          <a href="">
+                          <a href="#">
                           <span><img src="<?php print $path; ?>/subjects/provisional-3.jpg"></span>
                             <h6>January 1, 2015</h6>
                             <h5>Tsopo Monastery</h5>
@@ -123,14 +123,14 @@
       <div id="slide-three" class="feature-slide tab-pane fade">
         <div class="carousel-header go-to-link">
           <span>Featured Places: Tibet Autonomous Region</span>
-          <a href="/collections">View All Collections</a>
+          <a href="#">View All Collections</a>
         </div>
         <div id="feature-slide-detail" class="feature-slide-detail">
           <div class="container-fluid">
             <div class="row">
                   <div class="feature-slide-detail-content col-xs-12 col-sm-7 col-md-6 col-lg-7">
                     <div><h3 class="feature-slide-title">
-                      <a href="collection/tibetan-and-himalayan-library">
+                      <a href="#">
                        <span class="icon shanticon-stack"></span>
                           Royal Government of Bhutan Collections
                       </a>
@@ -143,7 +143,7 @@
                   <div class="carousel-related-resource-group col-xs-12 col-sm-5 col-md-6 col-lg-5 ">
                     <div class="row">
                         <div class="col-xs-6 col-md-4 carousel-resource-item">
-                          <a href="">
+                          <a href="#">
                           <span><img src="<?php print $path; ?>/subjects/bhutan-1.jpg"></span>
                             <h6>January 1, 2015</h6>
                             <h5>Centenary Celebration</h5>
@@ -151,7 +151,7 @@
                           </a>
                         </div>
                         <div class="col-xs-6 col-md-4 carousel-resource-item">
-                          <a href="">
+                          <a href="#">
                           <span><img src="<?php print $path; ?>/subjects/bhutan-2.jpg"></span>
                             <h6>January 1, 2015</h6>
                             <h5>Ministry of Trade and Industry</h5>
@@ -159,7 +159,7 @@
                           </a>
                         </div>
                         <div class="hidden-xs hidden-sm col-md-4 carousel-resource-item">
-                          <a href="">
+                          <a href="#">
                           <span><img src="<?php print $path; ?>/subjects/bhutan-3.jpg"></span>
                             <h6>January 1, 2015</h6>
                             <h5>Royal University of Bhutan</h5>
@@ -182,14 +182,14 @@
 			<div id="slide-four" class="feature-slide tab-pane fade">
         <div class="carousel-header go-to-link">
           <span>Featured Places: Tibet Autonomous Region</span>
-          <a href="/collections">View All Collections</a>
+          <a href="#">View All Collections</a>
         </div>
         <div id="feature-slide-detail" class="feature-slide-detail">
           <div class="container-fluid">
             <div class="row">
                   <div class="feature-slide-detail-content col-xs-12 col-sm-7 col-md-6 col-lg-7">
                     <div><h3 class="feature-slide-title">
-                      <a href="collection/tibetan-and-himalayan-library">
+                      <a href="#">
                        <span class="icon shanticon-stack"></span>
                           Tibetan Contemporary Art
                       </a>
@@ -202,7 +202,7 @@
                   <div class="carousel-related-resource-group col-xs-12 col-sm-5 col-md-6 col-lg-5 ">
                     <div class="row">
                         <div class="col-xs-6 col-md-4 carousel-resource-item">
-                          <a href="">
+                          <a href="#">
                           <span><img src="<?php print $path; ?>/subjects/tibart-1.jpg"></span>
                             <h6>January 1, 2015</h6>
                             <h5>Ang Sang</h5>
@@ -210,7 +210,7 @@
                           </a>
                         </div>
                         <div class="col-xs-6 col-md-4 carousel-resource-item">
-                          <a href="">
+                          <a href="#">
                           <span><img src="<?php print $path; ?>/subjects/tibart-2.jpg"></span>
                             <h6>January 1, 2015</h6>
                             <h5>Dedron</h5>
@@ -218,7 +218,7 @@
                           </a>
                         </div>
                         <div class="hidden-xs hidden-sm col-md-4 carousel-resource-item">
-                          <a href="">
+                          <a href="#">
                           <span><img src="<?php print $path; ?>/subjects/tibart-3.jpg"></span>
                             <h6>January 1, 2015</h6>
                             <h5>Ngawang-Jigme</h5>
@@ -238,17 +238,17 @@
       </div><!-- END slide four -->
 
 
-			<div id="slide-five" class="feature-slide tab-pane fade active in">
+			<div id="slide-five" class="feature-slide tab-pane fade">
         <div class="carousel-header go-to-link">
           <span>Featured Subjects: Collections</span>
-          <a href="/collections">View All Collections</a>
+          <a href="#">View All Collections</a>
         </div>
         <div id="feature-slide-detail" class="feature-slide-detail">
           <div class="container-fluid">
             <div class="row">
 		              <div class="feature-slide-detail-content col-xs-12 col-sm-7 col-md-6 col-lg-7">
 		                <div><h3 class="feature-slide-title">
-		                  <a href="collection/tibetan-and-himalayan-library">
+		                  <a href="#">
 		                   <span class="icon shanticon-stack"></span>
 		                      Central Tibet Collections
 		                  </a>
@@ -261,7 +261,7 @@
 		              <div class="carousel-related-resource-group col-xs-12 col-sm-5 col-md-6 col-lg-5">
 		                <div class="row">
 		                  <div class="col-xs-6 col-md-4 carousel-resource-item">
-		                    <a href="">
+		                    <a href="#">
 		                    <span><img src="<?php print $path; ?>/subjects/central-1.jpg"></span>
 		                      <h6>January 1, 2015</h6>
 		                      <h5>Basum Island</h5>
@@ -269,7 +269,7 @@
 		                    </a>
 		                  </div>
 		                  <div class="col-xs-6 col-md-4 carousel-resource-item">
-		                    <a href="">
+		                    <a href="#">
 		                    <span><img src="<?php print $path; ?>/subjects/central-2.jpg"></span>
 		                      <h6>January 1, 2015</h6>
 		                      <h5>Duma</h5>
@@ -277,7 +277,7 @@
 		                    </a>
 		                  </div>
 		                  <div class="hidden-xs hidden-sm col-md-4 carousel-resource-item">
-		                    <a href="">
+		                    <a href="#">
 		                    <span><img src="<?php print $path; ?>/subjects/central-3.jpg"></span>
 		                      <h6>January 1, 2015</h6>
 		                      <h5>Mindrölling Monastery</h5>
@@ -300,14 +300,14 @@
 			<div id="slide-six" class="feature-slide tab-pane fade">
         <div class="carousel-header go-to-link">
           <span>Featured Places: Tibet Autonomous Region</span>
-          <a href="/collections">View All Collections</a>
+          <a href="#">View All Collections</a>
         </div>
         <div id="feature-slide-detail" class="feature-slide-detail">
           <div class="container-fluid">
             <div class="row">
                   <div class="feature-slide-detail-content col-xs-12 col-sm-7 col-md-6 col-lg-7">
                     <div><h3 class="feature-slide-title">
-                      <a href="collection/tibetan-and-himalayan-library">
+                      <a href="#">
                        <span class="icon shanticon-stack"></span>
                           Eastern Tibet Collections
                       </a>
@@ -320,7 +320,7 @@
                   <div class="carousel-related-resource-group col-xs-12 col-sm-5 col-md-6 col-lg-5 ">
                     <div class="row">
                         <div class="col-xs-6 col-md-4 carousel-resource-item">
-                          <a href="">
+                          <a href="#">
                           <span><img src="<?php print $path; ?>/subjects/eastern-1.jpg"></span>
                             <h6>January 1, 2015</h6>
                             <h5>Degé to Kandzé</h5>
@@ -328,7 +328,7 @@
                           </a>
                         </div>
                         <div class="col-xs-6 col-md-4 carousel-resource-item">
-                          <a href="">
+                          <a href="#">
                           <span><img src="<?php print $path; ?>/subjects/eastern-2.jpg"></span>
                             <h6>January 1, 2015</h6>
                             <h5>Drango to Serta</h5>
@@ -336,7 +336,7 @@
                           </a>
                         </div>
                         <div class="hidden-xs hidden-sm col-md-4 carousel-resource-item">
-                          <a href="">
+                          <a href="#">
                           <span><img src="<?php print $path; ?>/subjects/eastern-3.jpg"></span>
                             <h6>January 1, 2015</h6>
                             <h5>Larung Gar</h5>
