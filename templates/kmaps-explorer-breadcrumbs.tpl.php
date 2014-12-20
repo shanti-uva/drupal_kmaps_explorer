9f2c54016c3624d4fdac438cd1d2b9466aeca6a5
Gerard Ketuma <me@gerardketuma.com>
2014-12-20 04:57:54 -0600

    Add title and breadcrumbs

---
+++ templates/kmaps-explorer-breadcrumbs.tpl.php
@@ -0,0 +1,9 @@
+<li>
+  <a href="<?php print base_path(); ?>"><?php print ucfirst($app); ?>: </a>
+</li>
+<?php foreach($data as $aItem): ?>
+  <li>
+    <a href="<?php print base_path(); ?><?php print $app; ?>/<?php print $aItem->id; ?>/overview/nojs" class="use-ajax"><?php print $aItem->header; ?></a>
+    <span class="icon shanticon-arrow3-right"></span>
+  </li>
+<?php endforeach; ?>
