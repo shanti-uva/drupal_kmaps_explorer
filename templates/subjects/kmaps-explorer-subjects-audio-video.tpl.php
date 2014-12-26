36ba17aff5ea47d747e42dd64d6e8c5ed7538ac6
mferrara <mf8yk@virginia.edu>
2014-12-26 17:09:47 -0600

    fixing Video thumbnail markup & fields

--- templates/subjects/kmaps-explorer-subjects-audio-video.tpl.php
+++ templates/subjects/kmaps-explorer-subjects-audio-video.tpl.php
@@ -1,30 +1,33 @@
 <ul class="related-audio-video shanti-gallery">
   <?php foreach($data as $aItem): ?>
-    <li class="shanti-thumbnail video col-lg-2 col-md-3 col-sm-4 col-xs-12">
+    <li class="shanti-thumbnail video">
       <div class="shanti-thumbnail-image shanti-field-video">
         <a href="<?php print base_path() . 'audio-video/node/' . $aItem->nid; ?>/nojs" class="shanti-thumbnail-link use-ajax">
           <span class="overlay"><span cla;ss="icon"></span></span>
           <img src="<?php print $aItem->thumbnail; ?>/width/360/height/270/type/2/bgcolor/000000" alt="Video" typeof="foaf:Image" class="k-no-rotate">
-          <i class="shanticon-video thumbtype"></i>
+          <span class="icon shanticon-video"></span>
         </a>
       </div>
       <div class="shanti-thumbnail-info">
         <div class="body-wrap">
-          <div class="shanti-thumbnail-field shanti-field-created">
-            <span class="shanti-field-content">
-              <?php print date('F j, Y', $aItem->created); ?>
-            </span>
             <div class="shanti-thumbnail-field shanti-field-title">
               <span class="field-content">
                 <a href="<?php print base_path() . 'audio-video/node/' . $aItem->nid; ?>/nojs" class="shanti-thumbnail-link use-ajax">
                   <?php print $aItem->title; ?>
                 </a>
               </span>
+            </div>                   
+            <div class="shanti-thumbnail-field shanti-field-created">
+              <span class="shanti-field-content">
+                <?php print date('F j, Y', $aItem->created); ?>
+              </span>
             </div>
             <div class="shanti-thumbnail-field shanti-field-duration">
               <span class="field-content"><?php print $aItem->duration->formatted; ?></span>
             </div>
-          </div>
+            <div class="shanti-thumbnail-field shanti-field-group-audience">
+              <span class="field-content"></span>
+            </div>
         </div>
         <div class="footer-wrap">
         </div>
