89d0496f1e28abc9aaccafba6fa08c3fb6fb62d8
Gerard Ketuma <me@gerardketuma.com>
2015-02-03 07:10:54 -0600

    Add popovers for subjects/subjects

--- templates/subjects/kmaps-explorer-subjects-subjects.tpl.php
+++ templates/subjects/kmaps-explorer-subjects-subjects.tpl.php
@@ -4,13 +4,21 @@
     <li><?php print ucfirst($aItem->label); ?> (<?php print count($aItem->features); ?>):</li>
     <ul>
       <?php foreach($aItem->features as $bItem): ?>
+        <?php
+          $bcrumbs = array();
+          foreach($bItem->ancestors as $cItem) {
+            $bcrumbs[$cItem->id] = '<a href="#">' . ucfirst($cItem->header) . '</a>';
+          }
+
+          $desc = empty($bItem->caption) ? '<p>Currently no description available</p>' : '<p>' . $bItem->caption . '</p>';
+        ?>
         <li>
-          <?php print shanti_sarvaka_info_popover(array(
+        <?php print shanti_sarvaka_info_popover(array(
         'label' => $bItem->header . ' (From the General Perspective)',
-        'desc' => '<p>Currently no description available</p>',
+        'desc' => $desc,
         'tree' => array(
-          'label' => 'Feature Types',
-          'items' => array('t' => ''),
+          'label' => 'Subjects',
+          'items' => $bcrumbs,
         ),
         'links' => array(
           'Full Entry' => array(
