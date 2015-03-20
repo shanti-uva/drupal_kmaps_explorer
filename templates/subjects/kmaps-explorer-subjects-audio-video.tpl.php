0702d10f7333d19926972f014c15edf4f2664e05
Gerard Ketuma <me@gerardketuma.com>
2015-03-20 08:52:29 -0500

    Change audio-video to now use information from solr in contructing the pages

--- templates/subjects/kmaps-explorer-subjects-audio-video.tpl.php
+++ templates/subjects/kmaps-explorer-subjects-audio-video.tpl.php
@@ -2,9 +2,9 @@
   <?php foreach($data as $aItem): ?>
     <li class="shanti-thumbnail video">
       <div class="shanti-thumbnail-image shanti-field-video">
-        <a href="<?php print base_path() . $app . '/' . $main_id . '/audio-video-node/' . $aItem->nid . '/nojs'; ?>" class="shanti-thumbnail-link use-ajax">
+        <a href="<?php print base_path() . $app . '/' . $main_id . '/audio-video-node/' . $aItem->id . '/nojs'; ?>" class="shanti-thumbnail-link use-ajax">
           <span class="overlay"><span class="icon"></span></span>
-          <img src="<?php print $aItem->thumbnail; ?>/width/360/height/270/type/2/bgcolor/000000" alt="Video" typeof="foaf:Image" class="k-no-rotate">
+          <img src="<?php print $aItem->url_thumb; ?>" alt="Video" typeof="foaf:Image" class="k-no-rotate">
           <span class="icon shanticon-video"></span>
         </a>
       </div>
@@ -12,18 +12,18 @@
         <div class="body-wrap">
             <div class="shanti-thumbnail-field shanti-field-title">
               <span class="field-content">
-                <a href="<?php print base_path() . $app . '/' . $main_id . '/audio-video-node/' . $aItem->nid . '/nojs'; ?>" class="shanti-thumbnail-link use-ajax">
-                  <?php print $aItem->title; ?>
+                <a href="<?php print base_path() . $app . '/' . $main_id . '/audio-video-node/' . $aItem->id . '/nojs'; ?>" class="shanti-thumbnail-link use-ajax">
+                  <?php print $aItem->caption; ?>
                 </a>
               </span>
             </div>
             <div class="shanti-thumbnail-field shanti-field-created">
               <span class="shanti-field-content">
-                <?php print date('F j, Y', $aItem->created); ?>
+                <?php print date('F j, Y'); ?>
               </span>
             </div>
             <div class="shanti-thumbnail-field shanti-field-duration">
-              <span class="field-content"><?php print $aItem->duration->formatted; ?></span>
+              <span class="field-content"><?php //print $aItem->duration->formatted; ?></span>
             </div>
             <div class="shanti-thumbnail-field shanti-field-group-audience">
               <span class="field-content"></span>
