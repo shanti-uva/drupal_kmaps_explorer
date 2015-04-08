bf858990fabe2365eabfda25cf01f3cdabccc5e2
Gerard Ketuma <me@gerardketuma.com>
2015-04-08 08:21:21 -0500

    Change individual photos node to use mms api

--- templates/subjects/kmaps-explorer-subjects-photos-node.tpl.php
+++ templates/subjects/kmaps-explorer-subjects-photos-node.tpl.php
@@ -1,10 +1,17 @@
 <div class="related-photos">
-  <img src="<?php print $aItem->images[4]->url; ?>" alt="<?php print (count($aItem->captions) > 0 ? $aItem->captions[0]->title : ''); ?>">
-  <p><strong>Resource #:</strong> <?php print $aItem->id; ?></p>
-  <p><strong>Description:</strong></p>
-  <?php print (count($aItem->descriptions) > 0 ? $aItem->descriptions[0]->title : ''); ?>
-  <p><strong>Copyright holder:</strong> <?php print (count($aItem->copyrights) > 0 ? $aItem->copyrights[0]->copyright_holder->title : ''); ?></p>
-  <?php if(isset($aItem->photographer)): ?>
-    <p><strong>Photographer:</strong> <?php print (isset($aItem->photographer->fullname) ? $aItem->photographer->fullname : ''); ?></p>
+  <?php if(count($photo->picture->captions) > 0): ?>
+    <p><?php print $photo->picture->captions[0]->title; ?></p>
+  <?php endif; ?>
+  <figure>
+    <img src="<?php print $photo->picture->images[3]->url; ?>">
+    <?php if(isset($photo->picture->photographer)): ?>
+      <figcaption><strong>Photographer:</strong> <?php print $photo->picture->photographer->fullname; ?></figcaption>
+    <?php endif; ?>
+  </figure><br>
+  <?php if(isset($photo->picture->copyrights)): ?>
+    <p><strong>Copyright Holder:</strong> <?php print $photo->picture->copyrights[0]->copyright_holder->title; ?></p>
+  <?php endif; ?>
+  <?php if(isset($photo->picture->copyrights[0]->reproduction_type)): ?>
+    <p><strong>Reproduction Type:</strong> <?php print $photo->picture->copyrights[0]->reproduction_type->title; ?></p>
   <?php endif; ?>
 </div>
