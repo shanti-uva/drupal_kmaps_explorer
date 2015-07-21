2c61edd142a48d54a848053cc51140ddbf3dc9bf
Gerard Ketuma <me@gerardketuma.com>
2015-07-20 23:53:56 -0500

    Fix issue with google maps causing JS errors for related tabs in places

--- js/google_maps.js
+++ js/google_maps.js
@@ -2,38 +2,39 @@
   Drupal.behaviors.kmapsExplorerMaps = {
     attach: function (context, settings) {
 
-      var ctaLayer = new google.maps.KmlLayer({
-        url: 'http://places.thlib.org/features/gis_resources/' + Drupal.settings.kmaps_explorer.closest_fid_with_shapes + '.kmz'
-      });
+      if ($('#map-canvas').length) {
+        var ctaLayer = new google.maps.KmlLayer({
+          url: 'http://places.thlib.org/features/gis_resources/' + Drupal.settings.kmaps_explorer.closest_fid_with_shapes + '.kmz'
+        });
 
-      var mapOptions = {
-        zoom: 11,
-        scrollwheel: false,
-        center: ctaLayer.getDefaultViewport()
-      }
+        var mapOptions = {
+          zoom: 11,
+          scrollwheel: false,
+          center: ctaLayer.getDefaultViewport()
+        };
 
+        ctaLayer.setMap(new google.maps.Map(document.getElementById('map-canvas'), mapOptions));
 
-      ctaLayer.setMap(new google.maps.Map(document.getElementById('map-canvas'), mapOptions));
-
-      //Hide the openlayer maps
-      $('.openlayermap', context).hide();
-
-      //Listener functions for the map switch buttons.
-      $(".renderGmaps").click(function() {
+        //Hide the openlayer maps
         $('.openlayermap', context).hide();
-        $('#map-canvas', context).show();
-        $(this).addClass('active');
-        $('.renderOpenLayerMaps').removeClass('active');
-      });
 
-      $(".renderOpenLayerMaps").click(function() {
-        $('#map-canvas', context).hide();
-        var iframe = $('.openlayermap > iframe', context);
-        iframe.attr("src", iframe.data("src"));
-        $('.openlayermap', context).show();
-        $(this).addClass('active');
-        $('.renderGmaps').removeClass('active');
-      });
+        //Listener functions for the map switch buttons.
+        $(".renderGmaps").click(function() {
+          $('.openlayermap', context).hide();
+          $('#map-canvas', context).show();
+          $(this).addClass('active');
+          $('.renderOpenLayerMaps').removeClass('active');
+        });
+
+        $(".renderOpenLayerMaps").click(function() {
+          $('#map-canvas', context).hide();
+          var iframe = $('.openlayermap > iframe', context);
+          iframe.attr("src", iframe.data("src"));
+          $('.openlayermap', context).show();
+          $(this).addClass('active');
+          $('.renderGmaps').removeClass('active');
+        });
+      }
     }
   };
 })(jQuery);
