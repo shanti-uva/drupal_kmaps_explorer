cb3fe690ae7be6b0c58c366fe1660b3c2a8f4868
Edward Garrett <edward@pinedrop.com>
2016-09-10 16:06:03 +0100

    removing search and filtering from scope of tree in explorer navigator block

--- templates/kmaps-explorer-block.tpl.php
+++ templates/kmaps-explorer-block.tpl.php
@@ -1,34 +1,34 @@
 <section id="kmaps-search" role="search">
-    <!-- BEGIN Input section -->
-    <section class="input-section" style="display:none;">
-        <form class="form">
+  <!-- BEGIN view section -->
+  <section class="view-section">
+    <ul class="nav nav-tabs">
+      <li class="listview active"><a href=".listview" data-toggle="tab"><span class="icon shanticon-list"></span>Search</a></li>
+      <li class="treeview"><a href=".treeview" data-toggle="tab"><span
+            class="icon shanticon-tree"></span>Browse</a></li>
+    </ul>
+    <div class="tab-content">
+      <!-- TAB - list view -->
+      <div class="listview tab-pane active">
+        <!-- BEGIN Input section -->
+        <section class="input-section" style="display:none;">
+          <form class="form">
             <fieldset>
-                <div class="search-group">
-                    <?php print render($search); ?>
-                </div>
-                <section class="search-filters">
-                    <?php print render($filters); ?>
-                </section><!-- END seach filters -->
+              <div class="search-group">
+                <?php print render($search); ?>
+              </div>
+              <section class="search-filters">
+                <?php print render($filters); ?>
+              </section><!-- END seach filters -->
             </fieldset>
-        </form>
-    </section> <!-- END input section -->
-
-    <!-- BEGIN view section -->
-    <section class="view-section">
-        <ul class="nav nav-tabs">
-            <li class="treeview active"><a href=".treeview" data-toggle="tab"><span class="icon shanticon-tree"></span>Tree</a></li>
-            <li class="listview"><a href=".listview" data-toggle="tab"><span class="icon shanticon-list"></span>List</a></li>
-        </ul>
-        <div class="tab-content">
-            <!-- TAB - tree view -->
-            <div class="treeview tab-pane active">
-                <div id="tree" class="view-wrap"><!-- view-wrap controls tree container height --></div>
-            </div>
-            <!-- TAB - list view -->
-            <div class="listview tab-pane">
-                <div class="view-wrap"> <!-- view-wrap controls container height -->
-                </div>
-            </div>
+          </form>
+        </section> <!-- END input section -->
+        <div class="view-wrap"> <!-- view-wrap controls container height -->
         </div>
-    </section><!-- END view section -->
+      </div>
+      <!-- TAB - tree view -->
+      <div class="treeview tab-pane">
+        <div id="tree" class="view-wrap"><!-- view-wrap controls tree container height --></div>
+      </div>
+    </div>
+  </section><!-- END view section -->
 </section><!-- END kmaps-search -->
