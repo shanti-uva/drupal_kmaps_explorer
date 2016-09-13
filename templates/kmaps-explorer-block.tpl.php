4e9f89db4c95abbf1f1312f97a70bfc3c4b836d0
Edward Garrett <edward@pinedrop.com>
2016-09-13 11:58:44 +0100

    put explorer block input-section back above view-section (MANU-2537)

--- templates/kmaps-explorer-block.tpl.php
+++ templates/kmaps-explorer-block.tpl.php
@@ -1,19 +1,19 @@
 <section id="kmaps-search" role="search">
+  <!-- BEGIN Input section -->
+  <section class="input-section" style="display:none;">
+    <form class="form">
+      <fieldset>
+        <div class="search-group">
+          <?php print render($search); ?>
+        </div>
+        <section class="search-filters">
+          <?php print render($filters); ?>
+        </section><!-- END seach filters -->
+      </fieldset>
+    </form>
+  </section> <!-- END input section -->
   <!-- BEGIN view section -->
   <section class="view-section">
-    <!-- BEGIN Input section -->
-    <section class="input-section" style="display:none;">
-      <form class="form">
-        <fieldset>
-          <div class="search-group">
-            <?php print render($search); ?>
-          </div>
-          <section class="search-filters">
-            <?php print render($filters); ?>
-          </section><!-- END seach filters -->
-        </fieldset>
-      </form>
-    </section> <!-- END input section -->
     <ul class="nav nav-tabs">
       <li class="listview active"><a href=".listview" data-toggle="tab"><span class="icon shanticon-list"></span>Results</a></li>
       <li class="treeview"><a href=".treeview" data-toggle="tab"><span
@@ -22,13 +22,6 @@
     <div class="tab-content">
       <!-- TAB - list view -->
       <div class="listview tab-pane active">
-        <!-- BEGIN Input section -->
-        <!-- <section class="input-section" style="display:none;">
-          <form class="form">
-            <fieldset>
-            </fieldset>
-          </form>
-        </section> --> <!-- END input section -->
         <div class="view-wrap"> <!-- view-wrap controls container height -->
         </div>
       </div>
